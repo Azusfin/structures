7c24721518410d0202b64234ab72aca82a33dcd5
Azusfin <Azusfin@users.noreply.github.com>
2022-03-20 14:54:56 +0700

    feat: BitSet
    A static-length array to work with bytes in bit-level

---
+++ src/BitSet.ts
@@ -0,0 +1,98 @@
+export class BitSet {
+    public readonly bytes: Uint8Array
+    public readonly size: number
+
+    constructor(bits: number) {
+        if (bits < 1) {
+            throw new RangeError("Bitset size must be atleast 1")
+        }
+
+        this.bytes = new Uint8Array(Math.ceil(bits / 8))
+        this.size = bits
+    }
+
+    public get byteLength(): number {
+        return this.bytes.byteLength
+    }
+
+    public set(bit: number): void {
+        if (bit >= this.size) {
+            throw new RangeError("Bit index exceeds bitset size")
+        }
+
+        const index = this.#bitIndex(bit)
+        const value = this.#bitValue(bit)
+
+        this.bytes[index] |= value
+    }
+
+    public get(bit: number): boolean {
+        if (bit >= this.size) {
+            throw new RangeError("Bit index exceeds bitset size")
+        }
+
+        const index = this.#bitIndex(bit)
+        const value = this.#bitValue(bit)
+
+        return (this.bytes[index] & value) !== 0
+    }
+
+    public flip(bit: number): void {
+        if (bit >= this.size) {
+            throw new RangeError("Bit index exceeds bitset size")
+        }
+
+        const index = this.#bitIndex(bit)
+        const value = this.#bitValue(bit)
+
+        this.bytes[index] ^= value
+    }
+
+    public clear(bit: number): void {
+        if (bit >= this.size) {
+            throw new RangeError("Bit index exceeds bitset size")
+        }
+
+        const index = this.#bitIndex(bit)
+        const value = this.#bitValue(bit)
+
+        if ((this.bytes[index] & value) !== 0) {
+            this.bytes[index] ^= value
+        }
+    }
+
+    public clone(): BitSet {
+        const bitSet = new (this.constructor as typeof BitSet)(this.size)
+        bitSet.bytes.set(this.bytes)
+
+        return bitSet
+    }
+
+    public toString(): string {
+        const bits: number[] = []
+
+        let counter = 0
+
+        for (const byte of this.bytes) {
+            for (let i = 0; i < 8 && counter < this.size; i++) {
+                const value = this.#bitValue(i)
+
+                bits.push((byte & value) !== 0 ? 1 : 0)
+
+                counter++
+            }
+
+            if (counter >= this.size) break
+        }
+
+        return bits.reverse().join("")
+    }
+
+    #bitIndex(bit: number): number {
+        return Math.floor(bit / 8)
+    }
+
+    #bitValue(bit: number): number {
+        return 1 << bit % 8
+    }
+}
---
+++ test/BitSet.js
@@ -0,0 +1,32 @@
+const assert = require("assert/strict")
+const { BitSet } = require("../")
+
+describe("BitSet", function() {
+    const bitSet = new BitSet(10)
+    const bits = "0001000000"
+
+    it("Set 4", function() {
+        bitSet.set(3)
+    })
+
+    it("Get 4", function() {
+        assert.equal(bitSet.get(3), true)
+    })
+
+    it("Get 7", function() {
+        assert.equal(bitSet.get(6), false)
+    })
+
+    it("Clear 4", function() {
+        bitSet.clear(3)
+    })
+
+
+    it("Flip 7", function() {
+        bitSet.flip(6)
+    })
+
+    it("String of Bits", function() {
+        assert.equal(bitSet.toString(), bits)
+    })
+})
