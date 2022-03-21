ddde5980b2e4d4956217573e235d015aa90f86a9
Azusfin <Azusfin@users.noreply.github.com>
2022-03-21 09:17:46 +0700

    feat(RedBlackTree): add .find(key)

--- src/redblacktree/Tree.ts
+++ src/redblacktree/Tree.ts
@@ -1,5 +1,42 @@
-import { Key, Node } from "./Node"
+import { Color, Key, Node } from "./Node"
 
 export class Tree<K extends Key, V = unknown> {
     public root?: Node<K, V>
+    #nilNode: Node<K, V>
+
+    constructor() {
+        this.#nilNode = {
+            key: 0,
+            value: undefined,
+            color: Color.Black
+        } as unknown as Node<K, V>
+    }
+
+    public find(key: K): FindResult<V> {
+        let node: Node<K, V> | undefined = this.root
+
+        while (node && node !== this.#nilNode) {
+            if (node.key === key) {
+                return {
+                    found: true,
+                    value: node.value
+                }
+            }
+
+            if (key < node.key) {
+                node = node.left
+            } else {
+                node = node.right
+            }
+        }
+
+        return { found: false }
+    }
+}
+
+export type FindResult<V = unknown> = {
+    found: false
+} | {
+    found: true
+    value: V
 }
